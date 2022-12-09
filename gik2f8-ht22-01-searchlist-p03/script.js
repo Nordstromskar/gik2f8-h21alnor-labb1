48519067e40da0a5012edb06e08b5c300b21447e
Nordstromskar <alexnordis01@gmail.com>
2022-12-09 15:03:00 +0100

    Ändrade färger i bla index och fixade script

--- gik2f8-ht22-01-searchlist-p03/script.js
+++ gik2f8-ht22-01-searchlist-p03/script.js
@@ -19,10 +19,29 @@ searchField.addEventListener('keyup', (e) =>
 );
 
 function renderBookList(bookList) {
-  const existingElement = document.querySelector('.book-list');
+  const existingElement = document.querySelector('.book-list'); 
 
   const root = document.getElementById('root');
 
   existingElement && root.removeChild(existingElement);
+
   bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', BookList(bookList));
+
+    let bookUl = document.querySelector(".book-list");
+
+    if (bookUl){ /* kollar om listan inte är null*/
+        bookUl.addEventListener("mouseover", function(e) {
+            if (e.target && e.target.matches("li.book-list__item")) {
+                let bookId = e.target.value
+                let book = bookList.find(book => book.id === bookId)
+                bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', bookDetails(book)); /* visar omslagsbild för boken från bookdetails  */
+            }
+        });
+
+        bookUl.addEventListener("mouseout", function(e) { /* gör så att bilden med info försvinner om musen ej är över något i listan*/
+            let bookDetail = document.querySelector("#bookDetail");
+            if(bookDetail)
+                bookDetail.remove();
+        });
+    }
 }
